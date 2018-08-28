88152f9ec65e8fd4c2b054091bd6123aa0687e11
Johanne Boisvilliers <johanne.boisvilliers@gmail.com>
2018-08-28 18:51:05 +0200

    Add a toolbar on top of the screen with 'search' and right 'menu' icons

---
+++ app/src/main/java/com/example/jbois/mynews/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.jbois.mynews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+    // That method configure the toolbar (serialize & sets it)
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
+    }
+}
