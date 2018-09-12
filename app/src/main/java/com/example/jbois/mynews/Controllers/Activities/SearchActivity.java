6162b365cab13e240fc304fe2b09e209605bf83b
jograph <johanne.boisvilliers@gmail.com>
2018-09-12 14:25:31 +0200

    Create the search and notification prototypes//SearchActivity now load a fragment containing the prototype

--- app/src/main/java/com/example/jbois/mynews/Controllers/Activities/SearchActivity.java
+++ app/src/main/java/com/example/jbois/mynews/Controllers/Activities/SearchActivity.java
@@ -1,12 +1,12 @@
 package com.example.jbois.mynews.Controllers.Activities;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import com.example.jbois.mynews.Controllers.Fragments.SearchFragment;
 import com.example.jbois.mynews.R;
 
 import butterknife.BindView;
@@ -15,6 +15,10 @@ import butterknife.ButterKnife;
 public class SearchActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar) Toolbar mToolbar;
+
+    private SearchFragment mSearchFragment;
+    private String mPageTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +26,36 @@ public class SearchActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         this.setPageTitle();
+        this.configureAndShowSearchFragment();
+        //this.configureFragmentContent();
     }
     //Set the title in terms of what button user has pressed on the MainActivity toolbar and set the home button visible
     private void setPageTitle(){
-        String pageTitle = getIntent().getStringExtra("pageTitle");
+        mPageTitle= getIntent().getStringExtra("pageTitle");
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle(pageTitle);
+        getSupportActionBar().setTitle(mPageTitle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void configureAndShowSearchFragment(){
+        //Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
+        mSearchFragment = (SearchFragment) getSupportFragmentManager().findFragmentById(R.id.search_fragment_container);
+
+        if (mSearchFragment == null) {
+            //Create new main fragment
+            mSearchFragment = new SearchFragment();
+            //Add it to FrameLayout container
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.search_fragment_container, mSearchFragment)
+                    .commit();
+        }
+    }
+
+    private void configureFragmentContent (){
+        if(mPageTitle.equals("Search Articles")){
+        }else{
+
+        }
     }
 
 }
